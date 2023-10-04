37771c47a6258bb8cf8662026ef71e0c0a321611
jmrm81992 <jmrm81992@gmail.com>
2023-10-04 12:10:24 +0200

    añadir valores a tabla

--- producto/src/main/resources/META-INF/create.sql
+++ producto/src/main/resources/META-INF/create.sql
@@ -5,3 +5,16 @@ CREATE TABLE pedidosdb.productos (
 	stock INT NULL,
 	CONSTRAINT productos_pk PRIMARY KEY (codigo_producto)
 )
+
+INSERT INTO pedidosdb.productos
+(codigo_producto, producto, precio, stock)
+VALUES(1, 'Galletas', 5.0, 10);
+INSERT INTO pedidosdb.productos
+(codigo_producto, producto, precio, stock)
+VALUES(2, 'Leche', 2.0, 50);
+INSERT INTO pedidosdb.productos
+(codigo_producto, producto, precio, stock)
+VALUES(3, 'Cereales', 3.0, 30);
+INSERT INTO pedidosdb.productos
+(codigo_producto, producto, precio, stock)
+VALUES(4, 'Pan', 4.0, 25);
